bad145818316a3d335ccc5bebc8c1f2b602748be
katopz <katopz@gmail.com>
2017-02-02 15:11:05 +0700

    Move pure gql example to another file.

--- app/graphql/schema.js
+++ app/graphql/schema.js
@@ -1,23 +1,3 @@
-/*
-const graphql = require('graphql')
-const GraphQLSchema = graphql.GraphQLSchema
-const GraphQLObjectType = graphql.GraphQLObjectType
-const GraphQLString = graphql.GraphQLString
-
-module.exports = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        }
-      }
-    }
-  })
-});
-*/
 const { makeExecutableSchema } = require('graphql-tools')
 
 const rootSchema = [`
