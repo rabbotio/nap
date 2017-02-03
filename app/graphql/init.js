f68d1d4350287bde8da43af1afa9a3ad8c86c08e
katopz <katopz@gmail.com>
2017-02-03 12:29:52 +0700

    Refactor

---
+++ app/graphql/init.js
@@ -0,0 +1,34 @@
+const getSchema = (schemaURI, noCache) => {
+  // Use no cache while development.
+  noCache && delete require.cache[require.resolve(schemaURI)];
+
+  // Serve
+  return require(schemaURI)
+}
+
+const init = (app) => {
+  const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
+  const GRAPHQL_SCHEMA = process.env.GRAPHQL_SCHEMA || './schema.js'
+  const GRAPHQL_PRETTY = (process.env.GRAPHQL_PRETTY === '1') || IS_DEVELOPMENT
+  const GRAPHIQL_SUPPORT = (process.env.GRAPHIQL_SUPPORT === '1') || IS_DEVELOPMENT
+  const GRAPHQL_NOCACHE_SCHEMA = (process.env.GRAPHQL_NOCACHE_SCHEMA === '1') || IS_DEVELOPMENT
+
+  console.log('Starting GraphQL...');
+  console.log(`IS_DEVELOPMENT:${IS_DEVELOPMENT}`);
+  console.log(`GRAPHQL_SCHEMA:${GRAPHQL_SCHEMA}`);
+  console.log(`GRAPHQL_PRETTY:${GRAPHQL_PRETTY}`);
+  console.log(`GRAPHIQL_SUPPORT:${GRAPHIQL_SUPPORT}`);
+  console.log(`GRAPHQL_NOCACHE_SCHEMA:${GRAPHQL_NOCACHE_SCHEMA}`);
+
+  const graphQLHTTP = require('express-graphql');
+
+  app.use('/graphql', graphQLHTTP((request) => {
+    return {
+      graphiql: GRAPHIQL_SUPPORT,
+      pretty: GRAPHQL_PRETTY,
+      schema: getSchema(GRAPHQL_SCHEMA, GRAPHQL_NOCACHE_SCHEMA)
+    }
+  }));
+}
+
+module.exports = init
--- app/server.js
+++ app/server.js
@@ -2,64 +2,33 @@ const express = require('express')
 const next = require('next')
 
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
-const app = next({ IS_DEVELOPMENT })
-const handle = app.getRequestHandler()
+const _next = next({ IS_DEVELOPMENT })
+const handle = _next.getRequestHandler()
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000
 
-app.prepare()
+_next.prepare()
   .then(() => {
-    const server = express()
+    const app = express()
 
     // GraphQL
     const GRAPHQL_SUPPORT = process.env.GRAPHQL_SUPPORT === '1'
-    const GRAPHQL_SCHEMA = process.env.GRAPHQL_SCHEMA || './graphql/schema.js'
-    const GRAPHQL_PRETTY = (process.env.GRAPHQL_PRETTY === '1') || IS_DEVELOPMENT
-    const GRAPHIQL_SUPPORT = (process.env.GRAPHIQL_SUPPORT === '1') || IS_DEVELOPMENT
-    const GRAPHQL_NOCACHE_SCHEMA = (process.env.GRAPHQL_NOCACHE_SCHEMA === '1') || IS_DEVELOPMENT
-
-    console.log('Starting GraphQL...');
-    console.log(`IS_DEVELOPMENT:${IS_DEVELOPMENT}`);
-    console.log(`GRAPHQL_SUPPORT:${GRAPHQL_SUPPORT}`);
-    console.log(`GRAPHQL_SCHEMA:${GRAPHQL_SCHEMA}`);
-    console.log(`GRAPHQL_PRETTY:${GRAPHQL_PRETTY}`);
-    console.log(`GRAPHIQL_SUPPORT:${GRAPHIQL_SUPPORT}`);
-    console.log(`GRAPHQL_NOCACHE_SCHEMA:${GRAPHQL_NOCACHE_SCHEMA}`);
-
-    if (GRAPHQL_SUPPORT) {
-
-      function getSchema() {
-        if (GRAPHQL_NOCACHE_SCHEMA) {
-          delete require.cache[require.resolve(GRAPHQL_SCHEMA)];
-        }
-
-        return require(GRAPHQL_SCHEMA);
-      }
-
-      const graphQLHTTP = require('express-graphql');
-      server.use('/graphql', graphQLHTTP((request) => {
-        return {
-          graphiql: GRAPHIQL_SUPPORT,
-          pretty: GRAPHQL_PRETTY,
-          schema: getSchema()
-        }
-      }));
-    }
+    GRAPHQL_SUPPORT && require('./graphql/init')(app)
 
     // Static
-    server.get('/a', (req, res) => {
-      return app.render(req, res, '/b', req.query)
+    app.get('/a', (req, res) => {
+      return _next.render(req, res, '/b', req.query)
     })
 
-    server.get('/b', (req, res) => {
-      return app.render(req, res, '/a', req.query)
+    app.get('/b', (req, res) => {
+      return _next.render(req, res, '/a', req.query)
     })
 
-    server.get('*', (req, res) => {
+    app.get('*', (req, res) => {
       return handle(req, res)
     })
 
-    server.listen(HTTP_PORT, (err) => {
+    app.listen(HTTP_PORT, (err) => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
