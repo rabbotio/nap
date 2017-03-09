f42c42374bb0a87a8c670682f1153a13a3c194f1
katopz <katopz@gmail.com>
2017-03-09 17:37:02 +0700

    Deprecate Installation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import App from '../components/App'
-import Installation from '../components/Installation'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import withData from '../lib/withData'
 
 export default withData((props) => (
   <App>
-    <Installation />
     <Header pathname={props.url.pathname} />
     <Submit />
   </App>
