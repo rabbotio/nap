46910a4387003eb5c7d07a800513fd0434200630
katopz <katopz@gmail.com>
2017-03-10 18:09:19 +0700

    Cleanup

--- graphql/UserSchema.js
+++ graphql/UserSchema.js
@@ -61,16 +61,6 @@ const createUser = userData => new Promise((resolve, reject) => {
   })
 })
 
-/*
-UserTC.addFields({
-  error: {
-    type: 'JSON',
-    description: 'Error',
-    resolve: (source, args, context) => ({ foo: 'bar'}),
-  }
-})
-*/
-
 // - - - - - - Exports - - - - - -
 
 module.exports = { User, UserTC, Provider, createUser }
