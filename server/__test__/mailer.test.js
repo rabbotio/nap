264638744ce2384c46b4b538100cf5c9e06c7309
Todsaporn Banjerdkit <katopz@gmail.com>
2017-04-28 14:36:16 +0700

    test(mailer): should return verification mail body

--- server/__test__/mailer.test.js
+++ server/__test__/mailer.test.js
@@ -1,8 +1,28 @@
 /* eslint-env jest */
+
+require('../config')
+
 describe('mailer', () => {
-  it('should return error if no API_KEY or DOMAIN provide', async () => {
+  it('should return error if no API_KEY or DOMAIN provide while verification', async () => {
     const { willSendVerification } = require('../mailer')
     await willSendVerification(null, null).catch(err => {
+      expect(err).toMatchSnapshot()
+    })
+  })
+
+  it('should return verification mail body', async () => {
+    const { createVerificationURL } = require('../passport-local')
+    const email = 'katopz@gmail.com'
+    const verificationURL = createVerificationURL('http://localhost:3000', 'token')
+
+    const { willSendVerification } = require('../mailer')
+    const payload = await willSendVerification(email, verificationURL)
+    expect(payload).toMatchSnapshot()
+  })
+
+  it('should return error if no API_KEY or DOMAIN provide while reset password', async () => {
+    const { willSendPasswordReset } = require('../mailer')
+    await willSendPasswordReset(null, null).catch(err => {
       expect(err).toMatchSnapshot()
     })
   })
