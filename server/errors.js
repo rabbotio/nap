d5e5ea5a5db440673081906bf35471f5f9613fa5
Todsaporn Banjerdkit <katopz@gmail.com>
2017-05-12 14:51:51 +0700

    feat(errors): Add guard verification

--- server/errors.js
+++ server/errors.js
@@ -13,34 +13,44 @@ const _COMMON_ERRORS = {
 const _push = (req, { code, message }) => req.nap.errors.push({ code, message })
 
 const onError = (req) => (...args) => {
-  let error
-
   // Error('foo')  
   if (args[0] instanceof Error) {
     _push(req, new GenericError(0, args[0].message))
-    return error
+    return
   }
 
   // 'foo'
   if (args[0] instanceof String) {
-    _push(req, new GenericError(0, 'Unknown error'))
-    return error
+    _push(req, new GenericError(0, args[0]))
+    return
   }
 
   // 403, 'foo'  
   if (args[0] instanceof Number && args[1] instanceof String) {
     _push(req, new GenericError(...args))
-    return error
+    return
   }
 
   // 403
   if (args[0] instanceof Number && args.length === 1) {
     _push(req, new GenericError(0, _COMMON_ERRORS[args[0]] || ''))
-    return error
+    return
   }
 }
 
+const guard = (arg, msg) => {
+  const is = require('is_js')
+  if (!arg) { throw new Error('Required : Object e.g. { foo }') }
+  if (is.not.existy(Object.values(arg)[0])) {
+    throw new GenericError(0, msg || `Required : ${Object.keys(arg)[0]}`)
+  }
+
+  return false
+}
+
 module.exports = {
+  GenericError,
   SESSION_EMPTY_ERROR: new GenericError(190, 'User has no session provide'),
-  onError
+  onError,
+  guard
 }
