3479c53f4e887dcbb7cedf478c2a7ad5c174f560
Todsaporn Banjerdkit <katopz@gmail.com>
2017-05-15 19:45:11 +0700

    test(errors): Add more test, fixed error check bug

--- server/__test__/errors.test.js
+++ server/__test__/errors.test.js
@@ -23,4 +23,44 @@ describe('errors', () => {
     expect(() => guard({ foo: null })).toThrow('Required : foo')
     expect(() => guard({ foo: undefined })).toThrow('Required : foo')
   })
+
+  it('should add error new generic error to request', () => {
+    const { onError } = require('../errors')
+    const req = { nap: { errors:[] } }
+    onError(req)(new Error('foo'))
+
+    expect(req.nap.errors[0]).toMatchSnapshot()
+  })
+
+  it('should add string as new generic error to request', () => {
+    const { onError } = require('../errors')
+    const req = { nap: { errors:[] } }
+    onError(req)('foo')
+
+    expect(req.nap.errors[0]).toMatchSnapshot()
+  })
+
+  it('should add code, string as new generic error to request', () => {
+    const { onError } = require('../errors')
+    const req = { nap: { errors:[] } }
+    onError(req)(403, 'foo')
+
+    expect(req.nap.errors[0]).toMatchSnapshot()
+  })
+
+  it('should add code as new generic error to request', () => {
+    const { onError } = require('../errors')
+    const req = { nap: { errors:[] } }
+    onError(req)(3)
+
+    expect(req.nap.errors[0]).toMatchSnapshot()
+  })
+
+  it('should add code with common error as new generic error to request', () => {
+    const { onError } = require('../errors')
+    const req = { nap: { errors:[] } }
+    onError(req)(403)
+
+    expect(req.nap.errors[0]).toMatchSnapshot()
+  })
 })
