f9e5b91747269c391510ee8058d8fc785a75bf3f
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-06-01 19:53:36 +0700

    test(AuthenResolver): Add test

--- server/graphql/resolvers/__test__/AuthenReslover.test.js
+++ server/graphql/resolvers/__test__/AuthenReslover.test.js
@@ -9,13 +9,123 @@ describe('AuthenResolver', () => {
       )
     }
 
-    const authen = require('../AuthenResolver')
+    const { willAuthen } = require('../AuthenResolver')
     const installationId = 'FOO_INSTALLATION_ID'
     const userObject = { id: 'FOO_USER_ID' }
     const provider = 'facebook'
 
-    const user = await authen.willAuthen(installationId, userObject, provider)
+    const user = await willAuthen(installationId, userObject, provider)
 
+    expect(user).toMatchSnapshot()
+  })
+
+  it('should able to login with Facebook', async () => {
+    const userData = {
+      _id: '58d0e20e7ff032b39c2a9a18',
+      role: 'user',
+    }
+
+    const context = { 
+      headers : { host: 'localhost:3000'},
+      nap : {
+        willLoginWithFacebook: async () => userData,
+        willCreateUser: async () => userData,
+        willInstallAndAuthen: async () => userData,
+      }
+    }
+
+    const accessToken = 'FACEBOOK_ACCESS_TOKEN'
+    const args = { accessToken }
+
+    const { loginWithFacebook } = require('../AuthenResolver')
+    const user = await loginWithFacebook({ context, args })    
+    expect(user).toMatchSnapshot()
+  })
+
+  it('should able to login', async () => {
+    const userData = {
+      _id: '58d0e20e7ff032b39c2a9a18',
+      role: 'user',
+    }
+
+    const context = { 
+      headers : { host: 'localhost:3000'},
+      nap : {
+        willLogin: async () => userData,
+        willInstallAndAuthen: async () => userData,
+      }
+    }
+    const email = 'foo@bar.com'
+    const password = 'password'
+    const args = { email, password }
+
+    const { login } = require('../AuthenResolver')
+    const user = await login({ context, args })    
+    expect(user).toMatchSnapshot()
+  })
+
+  it('should able to signup', async () => {
+    const userData = {
+      _id: '58d0e20e7ff032b39c2a9a18',
+      role: 'user',
+    }
+
+    const context = { 
+      headers : { host: 'localhost:3000'},
+      nap : {
+        willSignUp: async () => userData,
+        willCreateUser: async () => userData,
+        willInstallAndAuthen: async () => userData,
+      }
+    }
+    const email = 'foo@bar.com'
+    const password = 'password'
+    const args = { email, password }
+
+    // stub
+    global.NAP = {}
+
+    const { signup } = require('../AuthenResolver')
+    const user = await signup({ context, args })    
+    expect(user).toMatchSnapshot()
+  })
+
+  it('should able to forget password', async () => {
+    const context = { 
+      headers : { host: 'localhost:3000'},
+      nap : {
+        willResetPassword: async () => ({ 
+          status: 'WAIT_FOR_EMAIL_RESET'
+        }),
+      }
+    }
+    const email = 'foo@bar.com'
+    const args = { email }
+    const { forget } = require('../AuthenResolver')
+    const user = await forget({ context, args })    
+    expect(user).toMatchSnapshot()
+  })
+
+  it('should able to logout', async () => {
+    const authenData = {
+      isLoggedIn: false,
+      sessionToken: null
+    }
+
+    const context = { 
+      logout:() => {},
+      session: {
+        destroy:() => {}
+      },
+      nap : {
+        willLogout: async () => authenData,
+        session: authenData
+      },
+      token: 'SESSION_TOKEN'
+    }
+
+    const { logout } = require('../AuthenResolver')
+    const user = await logout({ context })    
     expect(user).toMatchSnapshot()
   })
 })
