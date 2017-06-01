9ffaa9ac0460236227c84516c05f93a65674908e
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-06-01 17:58:47 +0700

    refactor(authen-local-passport): Rename _willVerifyPassword to _validatePassword

--- server/authen-local-passport.js
+++ server/authen-local-passport.js
@@ -100,28 +100,21 @@ const _willMarkUserAsVerifiedByToken = async (token) => {
   return await _withVerifiedByEmail(user).save()
 }
 
-const _willVerifyPassword = async (password, hashed_password) => {
+const _validatePassword = async (password, hashed_password) => {
   // Guard
-  if (!password) {
-    throw new Error('Required : password')
-  }
-
-  // Guard
-  if (!hashed_password) {
-    throw new Error('Required : hashed password')
-  }
+  guard({ password })
+  guard({ hashed_password })
 
   // Password matched?
   const bcrypt = require('bcryptjs')
-  const isEqual = bcrypt.compareSync(password, hashed_password)
-  return isEqual
+  return bcrypt.compareSync(password, hashed_password)
 }
 
 const validateLocalStrategy = (email, password, done) => {
   // Find by email
   (async () => {
-    const user = await NAP.User.findOne({ email, verified: true })
-    const isPasswordMatch = user && await _willVerifyPassword(password, user.hashed_password)
+    const user = await NAP.User.findOne({ email, verified: true }).catch(err => err)
+    const isPasswordMatch = user && await _validatePassword(password, user.hashed_password).catch(err => err)
     return done(null, isPasswordMatch ? user : false)
   })()
 }
