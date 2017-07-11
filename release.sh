24b1877ddba0bb619b09035f6f4fe54dab68b65f
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 19:05:10 +0700

    feat(release): Release with tag log

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
-run() {
+pre() {
   # Ensure git flow exist
   command -v git-flow >/dev/null 2>&1 || brew install git-flow
 
   # Tag as version
   tag=$(grep '"version":' package.json | cut -d\" -f4)
+}
 
+run() {
   # Start release
   git checkout develop
   git push
