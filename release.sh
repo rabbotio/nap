cf50dc31f30a8a23bf9639931a594587810d426f
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 18:59:44 +0700

    fix(release): Cleanup unused, void error

--- release.sh
+++ release.sh
@@ -1,19 +1,24 @@
-# Ensure git flow exist
-command -v git-flow >/dev/null 2>&1 || brew install git-flow
+run() {
+  # Ensure git flow exist
+  command -v git-flow >/dev/null 2>&1 || brew install git-flow
 
-# Tag as version
-tag=$(grep '"version":' package.json | cut -d\" -f4)
+  # Tag as version
+  tag=$(grep '"version":' package.json | cut -d\" -f4)
 
-# TODO : gen from sem-vers
-echo $tag > ./msg.tmp
+  # Start release
+  git checkout develop
+  git push
+  git flow release start $tag
+  git flow release finish -m $tag $tag
+  git checkout master
+  git push
+  git checkout develop
+  git push
+  git branch -d release/$tag >/dev/null 2>&1
+}
 
-# Start release
-git checkout develop
-git push
-git flow release start $tag
-git flow release finish -m $tag $tag
-git checkout master
-git push
-git checkout develop
-git push
-git branch -d release/$tag
+# Process
+PROCESS='release'
+echo -e "$(tput setaf 3)[$PROCESS] : Begin$(tput sgr0)"
+run
+echo -e "$(tput setaf 2)[$PROCESS] : End$(tput sgr0)"
